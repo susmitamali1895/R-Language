7db9aff1a2eb716da00efef97bf8d5ac90e27199
susmitamali1895 <mali.susmita09@gmail.com>
2026-02-17 09:37:52 +0530

    Add files via upload

---
+++ Create Data Frame.R
@@ -0,0 +1,10 @@
+
+# Create Data Frame
+df <- data.frame(
+  Name = c("Amit", "Neha", "Ravi", "Priya", "Karan"),
+  Age = c(25, 30, 28, 24, 35),
+  Salary = c(30000, 45000, 40000, 28000, 52000)
+)
+
+print(df)
+
