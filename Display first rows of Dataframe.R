22a5e881022b972defc3d9784e9d84a1fa2ddd9c
susmitamali1895 <mali.susmita09@gmail.com>
2026-02-19 09:19:13 +0530

    Add files via upload

---
+++ Display first rows of Dataframe.R
@@ -0,0 +1,7 @@
+# Display First rows 
+df <- data.frame(
+  Name = c("Amit", "Neha", "Ravi", "Priya", "Karan"),
+  Age = c(25, 30, 28, 24, 35),
+  Salary = c(30000, 45000, 40000, 28000, 52000)
+)
+head(df)
